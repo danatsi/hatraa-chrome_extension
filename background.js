6fbe56eb95eb12687c03149a4fe14ababb8daf4e
danatsi <dana.tsir@gmail.com>
2019-05-27 17:28:20 +0300

    added alerts

--- background.js
+++ background.js
@@ -1,22 +1,89 @@
 const socket = io.connect('http://10.0.70.207:8000');
 
+const configAreas = [];
+configAreas.push({
+  cityId: 1,
+  cityName: 'תל אביב',
+  cityArea: 'מרכז',
+});
+configAreas.push({
+  cityId: 2,
+  cityName: 'רמת גן',
+  cityArea: 'מרכז',
+});
+configAreas.push({
+  cityId: 3,
+  cityName: 'אילת',
+  cityArea: 'דרום',
+});
+configAreas.push({
+  cityId: 4,
+  cityName: 'באר שבע',
+  cityArea: 'דרום',
+});
+configAreas.push({
+  cityId: 5,
+  cityName: 'חיפה',
+  cityArea: 'צפון',
+});
+configAreas.push({
+  cityId: 6,
+  cityName: 'חיפה',
+  cityArea: 'צפון',
+});
+configAreas.push({
+  cityId: 7,
+  cityName: 'שדרות',
+  cityArea: 'עוטף עזה',
+});
+configAreas.push({
+  cityId: 8,
+  cityName: 'זיקים',
+  cityArea: 'עוטף עזה',
+});
+configAreas.push({
+  cityId: 9,
+  cityName: 'בית שמש',
+  cityArea: 'שפלה',
+});
+configAreas.push({
+  cityId: 10,
+  cityName: 'רמלה',
+  cityArea: 'שפלה',
+});
+var bkg = chrome.extension.getBackgroundPage();
+
 function notify(data, title, callback) {
   var options = {
-    title: 'Alert!',
+    title: title,
     message: data,
     type: 'basic', // Which type of notification to display - https://developer.chrome.com/extensions/notifications#type-TemplateType
     iconUrl: 'alert.jpg', // A URL to the sender's avatar, app icon, or a thumbnail for image notifications.
   };
-
   // The first argument is the ID, if left blank it'll be automatically generated.
   // The second argument is an object of options. More here: https://developer.chrome.com/extensions/notifications#type-NotificationOptions
   return chrome.notifications.create('', options, callback);
 }
 
+const getAlertedCities = data => {
+  //notify(data.event, 'Testing', callback);
+  let citiesAlerted = configAreas.filter(function(city) {
+    return data.polygon.includes(city.cityId);
+  });
+
+  let alertMessage = 'שים לב התרעה בישובים: ';
+  citiesAlerted.forEach(city => {
+    alertMessage += city.cityName + ', ';
+  });
+  return alertMessage.substring(0, alertMessage.length - 1);
+};
+
 socket.on('missile', function(data) {
-  //   var obj = JSON.stringify(data);
-  //   var showObj = JSON.parse(obj);
-  notify(data.event, 'Testing', callback);
+  alert(getAlertedCities());
+});
+
+socket.on('nonErgant', function(data) {
+  notify(getAlertedCities(), 'שים לב!', callback);
 });
 
 callback = function callbackFunc() {};
--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ const io = require('socket.io'), server = io.listen(8000);
 let sequenceNumberByClient = new Map();
 const alert = {
   incidentId: '109',
-  event: 'missile',
+  event: 'nonErgant',
   category: 'security',
   areaDescription: 'cities',
-  polygon: '(109, 323, 104, 213)',
+  polygon: [1, 2, 3, 4],
 };
 // event fired every time a new client connects:
 server.on('connection', socket => {
   console.info(`Client connected [id=${socket.id}]`);
   // initialize this client's sequence number
   sequenceNumberByClient.set(socket, 1);
-  socket.emit('missile', 'you are connected!');
+  //socket.emit('missile', 'you are connected!');
 
   // when socket disconnects, remove it from the list:
   socket.on('disconnect', () => {
@@ -25,7 +25,7 @@ server.on('connection', socket => {
 // sends each client its current sequence number
 setInterval(() => {
   for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-    client.emit('missile', alert);
+    client.emit(alert.event, alert);
     sequenceNumberByClient.set(client, sequenceNumber + 1);
   }
-}, 1000);
+}, 30000);
