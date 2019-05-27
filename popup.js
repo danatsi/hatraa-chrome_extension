1b9fd7f5b92805d6cd9f4445576eb6c996fdb9af
danatsi <dana.tsir@gmail.com>
2019-05-27 15:19:26 +0300

    add all

--- popup.js
+++
@@ -1,9 +0,0 @@
-$('#getEvents').click(() => {
-  const socket = io.connect('http://10.0.70.207:8000');
-  socket.emit('getAllEvents');
-});
-
-socket.on('allEvents', function(data) {
-  alert(data);
-  socket.close();
-});
